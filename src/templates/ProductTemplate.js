0254a631d4e0f78dfb03b2a0d320702e9ae4e83d
J Galenti <thejustscience@gmail.com>
2021-05-20 22:10:57 -0600

    Change price by variant selection

--- src/templates/ProductTemplate.js
+++ src/templates/ProductTemplate.js
@@ -26,17 +26,44 @@ export default function ProductTemplate({data}) {
     const images = data.product.images
 
     const {getProductById} = React.useContext(CartContext)
+    const [product, setProduct] = React.useState(null)
+    const [selectedVariant, setSelectedVariant] = React.useState(null)
 
     React.useEffect(() => {
         getProductById(shopifyId).then((result) => {
-            console.log(result)
+            setProduct(result)
+            setSelectedVariant(result.variants[0])
         })
-    },[getProductById, shopifyId])
+    },[getProductById, shopifyId, setProduct])
+
+    const handleVariantChange = (e) => {
+        setSelectedVariant(product && product.variants.find(v => v.id === e.target.value))
+    }
 
     return (
         <div>
             <h1>{title}</h1>
             <p>{description}</p>
+            {product && product.availableForSale &&
+                <>
+                    <strong>File Types: </strong>
+                    <select onBlur={handleVariantChange}>
+                        {product && product.variants.map(v => (
+                            <option 
+                                key={v.id}
+                                value={v.id}
+                            >
+                                {v.title}
+                            </option>
+                        ))}
+                    </select>
+                    {!!selectedVariant && 
+                        <div>
+                            <span>$</span>{selectedVariant.price}
+                        </div>
+                    }
+                </>
+            }
             <ProductImageGallery images={images} />
         </div>
     )
